7b3013641acaca3e8b9a069af7229815f75a726b
Marco Kade <marco.kade@ts.berlin>
2025-03-21 15:44:00 +0100

    cleanup after fork for new WS2425

--- data/baeume/0-setup.js
+++
@@ -1,8 +0,0 @@
-// Map Container anbinden und konfigurieren
-var map = L.map('map-container', { 
-	center: [52.51, 13.45], 
-	zoom: 10 
-});
-
-map.zoomControl.setPosition('bottomright');
-// map.attributionControl.remove('');
