f0c0d3e164047f65a05b7d943d0879ef55a8c3c2
Marco Kade <marco.kade@ts.berlin>
2025-07-03 22:26:36 +0200

    repositioning back button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.createElement('a');
 button.setAttribute('id', 'back-button');
 button.setAttribute('href','../../index.html')
-button.setAttribute('style','font-family: sans-serif; font-weight: 400; color: hsl(215, 96%, 36%); padding: 8px 16px; border-radius: 0px; border: 2px solid #0059d8; background: #fff; box-shadow: 4px 4px 0px 0px #0059d8; text-decoration: none; position: fixed; top: 24px; z-index: 9000;');
+button.setAttribute('style','font-family: sans-serif; font-weight: 400; color: hsl(215, 96%, 36%); padding: 8px 16px; border-radius: 0px; border: 2px solid #0059d8; background: #fff; box-shadow: 4px 4px 0px 0px #0059d8; text-decoration: none; position: fixed; top: 24px; left: 24px; z-index: 9000;');
 
 button.text = '← Zurück zur Übersicht'
 
